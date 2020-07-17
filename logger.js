00c579341ec6baa8a54e8ac12eb930cb9027fa47
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-07-17 17:55:26 +0100

    works fine

--- logger.js
+++ logger.js
@@ -1,10 +1,27 @@
 const { createLogger, transports, format } = require('winston');
 const { colorize, combine, simple, timestamp } = format;
 
-// console.log(colorize, 'colorize');
 const logger = createLogger({
   transports: [
     new transports.Console({
+      level: 'info',
+      format: combine(
+        timestamp({ format: 'DD-MM-YYYY h:mm:ssa' }),
+        simple(),
+        colorize({
+          colors: {
+            info: 'blue',
+            errors: 'red',
+          },
+          level: true,
+          message: true,
+          all: true,
+        })
+      ),
+    }),
+
+    new transports.File({
+      filename: 'filelog',
       level: 'info',
       format: combine(
         timestamp({ format: 'DD-MM-YYYY h:mm:ssa' }),
